cb9b7784da0a314aa94910426926dcb535c264d9
Francois Romain <francoisromain@gmail.com>
2018-02-01 12:50:51 +0100

    feat: update version

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,69 @@
 <template>
-  <div>
-    <cml-ui></cml-ui>
-  </div>
+  <cml-app>
+    <div  class="container pt">
+      
+      <div class="blobs">
+        <cml-users class="blob-1-2 p border"></cml-users>
+        <cml-groups class="blob-1-2 p border"></cml-groups>
+      </div>
+      
+      <div class="blobs">
+        <cml-corpus class="blob-1-2 p border"></cml-corpus>
+        <cml-medias class="blob-1-2 p border"></cml-medias>
+        <cml-layers class="blob-1-2 p border"></cml-layers>
+        <cml-annotations class="blob-1-2 p border"></cml-annotations>
+      </div>
+
+      <cml-annotations-buttons class="mb"></cml-annotations-buttons>
+      <cml-annotations-timeline class="mb"></cml-annotations-timeline>
+      <cml-medias-controller class="mb"></cml-medias-controller>
+      <cml-medias-youtube class="mb"></cml-medias-youtube>
+      <cml-medias-videozoning class="mb"></cml-medias-videozoning>
+
+      <div class="blobs">
+        <cml-corpus class="blob-1-2 p border" uid="test"></cml-corpus>
+        <cml-medias class="blob-1-2 p border" uid="test"></cml-medias>
+        <cml-layers class="blob-1-2 p border" uid="test"></cml-layers>
+        <cml-annotations class="blob-1-2 p border" uid="test"></cml-annotations>
+      </div>
+
+    </div>
+  </cml-app>
 </template>
 
 <script>
-import cmlUi from 'camomile-ui'
+import {
+  cmlApp,
+  cmlUsers,
+  cmlGroups,
+  cmlCorpus,
+  cmlMedias,
+  cmlLayers,
+  cmlAnnotations,
+  cmlMediasYoutube,
+  cmlMediasVideo,
+  cmlMediasVideozoning,
+  cmlMediasController,
+  cmlAnnotationsButtons,
+  cmlAnnotationsTimeline
+} from 'camomile-ui'
 
 export default {
   name: 'my-app',
-
   components: {
-    cmlUi
+    cmlApp,
+    cmlUsers,
+    cmlGroups,
+    cmlCorpus,
+    cmlMedias,
+    cmlLayers,
+    cmlAnnotations,
+    cmlMediasYoutube,
+    cmlMediasVideo,
+    cmlMediasVideozoning,
+    cmlMediasController,
+    cmlAnnotationsButtons,
+    cmlAnnotationsTimeline
   }
 }
 </script>
-
